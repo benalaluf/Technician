2bbc2308a12966ed5663506c4300c832bb252de9
Your Name <you@example.com>
2025-07-01 19:13:43 +0300

    WIP: agent Wokring!

--- Technician/Agent.h
+++ Technician/Agent.h
@@ -9,7 +9,18 @@
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "Ws2_32.lib")
+
+#include <vector>
+
+#include "Exception.h"
+
 #define DEFAULT_PORT "1337"
+#define DEFAULT_BUFLEN 512
+
+class AgentException : public Exception {
+  public:
+    AgentException(int status, std::string funcName);
+};
 
 class Agent {
     SOCKET m_conn;
@@ -20,5 +31,10 @@ class Agent {
     void start();
     void bindSocket();
     void listenSocket();
+    SOCKET agentAccept();
     void handleClient();
+
+    void agentSend(SOCKET sock, char* data);
+    std::vector<char> agentRecv(SOCKET sock);
+
 };
