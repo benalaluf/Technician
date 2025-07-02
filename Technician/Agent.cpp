50c896a8c289a39a30d8d88928a60173fe8355b2
Ben <94129183+benalaluf@users.noreply.github.com>
2025-07-02 14:18:54 +0300

    Add: sockets

--- Technician/Agent.cpp
+++ Technician/Agent.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <vector>
 
-Agent::Agent() {
-    m_conn = INVALID_SOCKET;
+Agent::Agent(): m_conn() , m_addr(NULL){
+    //empty
 }
 
 void Agent::start() {
@@ -13,101 +13,43 @@ void Agent::start() {
     try {
         initSocket();
         std::cout << "init socket\n";
-        bindSocket();
+        m_conn.bind(m_addr);
         std::cout << "bind socket\n";
-        listenSocket();
+        m_conn.listen();
         std::cout << "Listen....\n";
         handleClient();
     } catch (const AgentException& error) {
+        std::cerr << "Error at: " << error.funcName << " Code: " << error.status << "\n ";
+        closesocket(error.m_socket);
+    
+    } catch (const SocketException& error) {
         std::cerr << "Error at: " << error.funcName << " Code: " << error.status << "\n ";
         closesocket(error.m_socket);
     }
 }
 
 void Agent::initSocket() {
-    struct addrinfo *ptr = NULL, hints;
-
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;
-
-    int result = getaddrinfo(NULL, DEFAULT_PORT, &hints, &m_addr);
-    if (result != 0) {
-        throw AgentException(result, "initSocket getaddrinfo", m_conn);
-    }
-
-    m_conn = socket(m_addr->ai_family, m_addr->ai_socktype, m_addr->ai_protocol);
-
-    if (m_conn == INVALID_SOCKET) {
-        freeaddrinfo(m_addr);
-        throw AgentException(WSAGetLastError(), "initSocket socket()", m_conn);
-    }
-}
-
-void Agent::bindSocket() {
-    int result = bind(m_conn, m_addr->ai_addr, (int)m_addr->ai_addrlen);
-    if (result == SOCKET_ERROR) {
-        freeaddrinfo(m_addr);
-        throw AgentException(WSAGetLastError(), "bindSocket bind()", m_conn);
-    }
-}
-
-void Agent::listenSocket() {
-    if (listen(m_conn, SOMAXCONN) == SOCKET_ERROR) {
-        throw AgentException(WSAGetLastError(), "listen", m_conn);
-    }
-}
-
-SOCKET Agent::agentAccept() {
-    SOCKET ClientSocket = INVALID_SOCKET;
-    ClientSocket = accept(m_conn, NULL, NULL);
-    if (ClientSocket == INVALID_SOCKET) {
-        throw AgentException(WSAGetLastError(), "aggentAccept", m_conn);
-    }
-    return ClientSocket;
+    m_addr = createAddress(DEFAULT_PORT);
+    m_conn = Socket(m_addr);
 }
 
 void Agent::handleClient() {
 
-    SOCKET ClientSocket = agentAccept();
+    Socket clientConn = m_conn.accept();
 
     while (1) {
-        auto recvbuf = agentRecv(ClientSocket);
+        auto recvbuf = clientConn.recv();
         std::cout << "Recv: " << recvbuf.data() << "\n";
         if (!strncmp(recvbuf.data(), REQUEST_STRING, recvbuf.size())) {
-            agentSend(ClientSocket, RESPONSE_STRING);
+            agentSend(clientConn, RESPONSE_STRING);
             std::cout << "SEND: " << RESPONSE_STRING << "\n";
         }
     }
 }
-void Agent::agentSend(SOCKET sock, std::string message) {
-    agentSend(sock, const_cast<char*>(message.c_str()), message.size());
-}
-void Agent::agentSend(SOCKET sock, char* data, size_t dataSize) {
-    int iSendResult = send(sock, data, dataSize, 0);
-    if (iSendResult == SOCKET_ERROR) {
-        throw AgentException(WSAGetLastError(), "send", sock);
-    }
+void Agent::agentSend(Socket sock, std::string message) {
+    sock.send(const_cast<char*>(message.c_str()), message.size());
 }
 
-std::vector<char> Agent::agentRecv(SOCKET sock) {
-    std::vector<char> recvbuf(DEFAULT_BUFLEN);
-    int recvResult;
-    int recvbuflen = DEFAULT_BUFLEN;
-
-    recvResult = recv(sock, recvbuf.data(), recvbuflen, 0);
-    if (recvResult <= 0) {
-        if (recvResult == 0) {
-            throw AgentException(recvResult, "connection closed", sock);
-        } else {
-            throw AgentException(recvResult, "agent Recv", sock);
-        }
-    }
-
-    return recvbuf;
-}
 
 AgentException::AgentException(int status, std::string funcName, SOCKET sock)
     : Exception(status, funcName), m_socket(sock) {
